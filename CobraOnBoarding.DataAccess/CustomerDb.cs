e0021795299be61d484f6ab31f9ce195d012f562
WilheimRy <wilheim.ry@gmail.com>
2017-03-01 14:08:53 +1300

    set up project

---
+++ CobraOnBoarding.DataAccess/CustomerDb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CobraOnBoarding.DataAccess
+{
+    public static class CustomerDb
+    {
+        public static List<Person> GetAllCustomers()
+        {
+            using (var context = new CobraOnboardingDbEntities())
+            {
+                return context.People.ToList();
+            }
+        }
+    }
+}
